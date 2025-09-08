fac99233c27f1a737ec1e4dab0f6b4833c8a5c1e
Michael Moen Allport <mmallport@gmail.com>
2025-09-08 16:29:20 +0200

    more wrangling

--- tests/integration/test_whodat.py
+++ tests/integration/test_whodat.py
@@ -1,26 +1,25 @@
-"""@pytest.mark.usefixtures("setup")
-@integration_test()
-def test_whodat() -> None:
-    df = pl.read_json(
-        "tests/data/data.json",
-        schema={
-            "navn": pl.String,
-            "foedselsdato": pl.String,
-            "bostedsadresse": pl.String,
-            "kjoenn": pl.String,
-        },
-    )
+# @pytest.mark.usefixtures("setup")
+# @integration_test()
+# def test_whodat() -> None:
+#     df = pl.read_json(
+#         "tests/data/data.json",
+#         schema={
+#             "navn": pl.String,
+#             "foedselsdato": pl.String,
+#             "bostedsadresse": pl.String,
+#             "kjoenn": pl.String,
+#         },
+#     )
 
-    result = (
-        Whodat.from_polars(df)
-        .search_fnr()
-        .with_search_strategy(variables=["navn"])
-        .with_search_strategy(
-            variables=["navn", "foedselsdato"],
-            inkluder_oppholdsadresse=True,
-            soek_fonetisk=False,
-            inkluder_doede=True,
-        )
-        .run()
-    )
-"""
+#     result = (
+#         Whodat.from_polars(df)
+#         .search_fnr()
+#         .with_search_strategy(variables=["navn"])
+#         .with_search_strategy(
+#             variables=["navn", "foedselsdato"],
+#             inkluder_oppholdsadresse=True,
+#             soek_fonetisk=False,
+#             inkluder_doede=True,
+#         )
+#         .run()
+#     )
