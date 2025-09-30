da3b86ccaf35bc9cac2c5c7035aafb688234c30a
Michael Moen Allport <mmallport@gmail.com>
2025-09-30 18:09:30 +0200

    Add integration test

--- tests/integration/test_whodat.py
+++ tests/integration/test_whodat.py
@@ -1,25 +1,32 @@
-# @pytest.mark.usefixtures("setup")
-# @integration_test()
-# def test_whodat() -> None:
-#     df = pl.read_json(
-#         "tests/data/data.json",
-#         schema={
-#             "navn": pl.String,
-#             "foedselsdato": pl.String,
-#             "bostedsadresse": pl.String,
-#             "kjoenn": pl.String,
-#         },
-#     )
+import polars as pl
+import pytest
 
-#     result = (
-#         Whodat.from_polars(df)
-#         .search_fnr()
-#         .with_search_strategy(variables=["navn"])
-#         .with_search_strategy(
-#             variables=["navn", "foedselsdato"],
-#             inkluder_oppholdsadresse=True,
-#             soek_fonetisk=False,
-#             inkluder_doede=True,
-#         )
-#         .run()
-#     )
+from dapla_whodat import Whodat
+from tests.integration.utils import integration_test
+
+
+@pytest.mark.usefixtures("setup")
+@integration_test()
+def test_whodat() -> None:
+    df = pl.read_json(
+        "tests/data/data.json",
+        schema={
+            "navn": pl.String,
+            "foedselsdato": pl.String,
+            "bostedsadresse": pl.String,
+            "kjoenn": pl.String,
+        },
+    )
+
+    result = (
+        Whodat.from_polars(df)
+        .search_fnr()
+        .with_search_strategy(variables=["navn"])
+        .with_search_strategy(
+            variables=["navn", "foedselsdato"],
+            inkluder_oppholdsadresse=True,
+            soek_fonetisk=False,
+            inkluder_doede=True,
+        )
+        .run()
+    )
