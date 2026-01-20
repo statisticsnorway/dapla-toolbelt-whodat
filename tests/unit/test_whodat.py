c5f80c2b3a9bd9ca605696860c76febb1efc845f
Linn Bjørnerud <125447659+linnbjornerud@users.noreply.github.com>
2026-01-20 11:07:42 +0100

    Add unit test for empty varlist and apply fixtures from conftest.py (#28)
    * Use conftest.py in integrations tests
    
    * Add code and unit test for expecting ValueError upon empty variable list

--- tests/unit/test_whodat.py
+++ tests/unit/test_whodat.py
@@ -1,6 +1,11 @@
+import polars as pl
+import pytest
+
 from dapla_whodat import Whodat
 
 
-def test_dummy() -> None:
-    Whodat()
-    assert True
+def test_should_fail_empty_var_list(df_personer_pl: pl.DataFrame) -> None:
+    with pytest.raises(ValueError):
+        Whodat.from_polars(df_personer_pl).search_fnr().with_search_strategy(
+            variables=[]
+        )
